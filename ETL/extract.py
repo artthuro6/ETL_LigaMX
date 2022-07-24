36f6f803488afeb575d5f1ffef009da25d233f33
carturo6 <arturo.carlos.nolasco@gmail.com>
2022-07-24 00:32:49 -0500

    Usando el notebook para ver los datos extraidos

--- ETL/extract.py
+++ ETL/extract.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np 
 import os 
 import requests
+from bs4 import BeautifulSoup
 
 #Urls de acceso
 url_1 = 'https://www.tudn.com/futbol/liga-mx/resultados'
@@ -18,9 +19,10 @@ def Extract():
 
 
 def _acces_to_page(url_1, url_2):
-    x1 = requests.get(url_1)
-    x2 = requests.get(url_2)
+    page_1 = requests.get(url_1)
+    #x2 = requests.get(url_2)
 
-    return (x1, x2)
+
+    return (page_1)
 
 
--- pipeline.py
+++ pipeline.py
@@ -1,22 +1,28 @@
 import pandas as pd 
 import numpy as np
+import ETL
 import logging
-logging.basicConfig(level=logging.INFO)
+import subprocess
 
-from extract import Extract
+from ETL.extract import Extract
 #from transform import Transform
 #from load import Load
+
+logging.basicConfig( filename='log.txt', filemode='a',
+                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                     level=logging.INFO, datefmt='%H:%M:%S')
 
 def pipeline():
     """
     Funcion para invocar el ETL
     """
-    
     #Recibiendo los datos de las paginas web
+    logging.info(' Provando el requests a las paginas ')
     data_web = Extract()
     logging.info(data_web[1].status_code)
     logging.info(data_web[0].status_code)
-    
+    logging.info(' Requests exitoso. ')
+
 
     #Transformando los datos
     #transform_data = Transform( data_web )
@@ -24,7 +30,8 @@ def pipeline():
     #Cargando los datos.
     #Load( transform_data )
 
-    
+    """Pendiente crear los logs diarios"""
+    #subprocess.run([ 'mv', 'log.txt', './LOG'  ])
 
 
 if __name__ == "__main__":
